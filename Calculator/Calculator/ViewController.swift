52b35ab4927ed31931a04c246b76d67eb612dd1d
Armand Grillet <armand.grillet@outlook.com>
2015-05-30 14:16:50 +0200

    First commit

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -19,57 +19,73 @@ class ViewController: UIViewController {
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
-            display.text = "\(newValue)"
+            if newValue % 1 != 0 {
+                display.text = "\(newValue)"
+            } else {
+                display.text = "\(Int(newValue))" // Remove the .O part
+            }
             userIsInTheMiddleOfTypingANumber = false
+            operandStack.append(displayValue)
+            addContentHistory(display.text!)
         }
     }
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         
-        if !(digit == "." && display.text!.rangeOfString(".") != nil) {
-            if userIsInTheMiddleOfTypingANumber {
-                display.text! += digit
+        if userIsInTheMiddleOfTypingANumber {
+            if (digit == "." && display.text!.rangeOfString(".") != nil) {
+                return
+            } else {
+               display.text! += digit
+            }
+        } else {
+            if digit == "." {
+                display.text = "0."
             } else {
                 display.text = digit
-                userIsInTheMiddleOfTypingANumber = true
             }
+            userIsInTheMiddleOfTypingANumber = true
         }
     }
     
     @IBAction func operate(sender: UIButton) {
+        enter()
         let operation = sender.currentTitle!
         addContentHistory(operation)
         switch operation {
-        case "×": performOperation {$0 * $1}
-        case "×": performOperation {$0 * $1}
-        case "÷": performOperation {$0 / $1}
-        case "+": performOperation {$0 + $1}
-        case "-": performOperation {$0 - $1}
-        case "sin": performOperation {sin($0)}
-        case "cos": performOperation {cos($0)}
-        case "π": performOperation {M_PI}
-        case "√": performOperation {sqrt($0)}
-        default: break
+            case "×": performOperation {$0 * $1}
+            case "×": performOperation {$0 * $1}
+            case "÷": performOperation {$0 / $1}
+            case "+": performOperation {$0 + $1}
+            case "-": performOperation {$0 - $1}
+            case "sin": performOperation {sin($0)}
+            case "cos": performOperation {cos($0)}
+            case "√": performOperation {sqrt($0)}
+            default: break
         }
     }
     
-    func performOperation(operation : (Double, Double) -> Double) {
+    @IBAction func conjure(sender: UIButton) {
+        let value = sender.currentTitle!
+        switch value {
+            case "π": display.text = String(stringInterpolationSegment: M_PI)
+            default: break
+        }
+    }
+    
+    private func performOperation(operation : (Double, Double) -> Double) {
         if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
+            let secondOperand = operandStack.removeLast()
+            let firstOperand = operandStack.removeLast()
+            displayValue = operation(firstOperand, secondOperand)
         }
     }
     
-    func performOperation(operation: (Double) -> Double) {
+    private func performOperation(operation: (Double) -> Double) {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
-            enter()
         }
-    }
-    
-    func performOperation(operation: () -> Double) {
-        displayValue = operation()
     }
     
     @IBAction func changeSign() {
@@ -77,13 +93,12 @@ class ViewController: UIViewController {
             self.display.text = "\(-1 * self.displayValue)"
         } else {
             displayValue = -1 * (operandStack.removeLast())
-            enter()
         }
     }
     
     @IBAction func clear() {
-        operandStack = Array<Double>()
         displayValue = 0
+        operandStack = Array<Double>()
         history.text = ""
     }
     
@@ -93,7 +108,7 @@ class ViewController: UIViewController {
         addContentHistory(display.text!)
     }
     
-    func addContentHistory(content: String) {
+    private func addContentHistory(content: String) {
         if history.text != "" {
             history.text! += " | " + content
         } else {
